8e24455139e66204fb29cb2b395ab68c7c6c2369
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2020-07-09 23:44:13 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Web Crypto Encryption Decryption Example
+
+Demonstrates a way to encrypt and decrypt data using the [Web Crypto API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API). Creates an AES-GCM key derived from a password based key (PBKDF2) for encryption. The AES-GCM key can be regenerated using the same password for decryption. The encrypted value is represented by a base64 string that includes the salt that was used when creating the password based key (PBKDF2), the initialization vector (iv) used for creating the AES key, and the encrypted content.
+
+## References
+
+- [Ernie Turner: Dodging Web Crypto API Landmines | Web Rebels 2018](https://www.youtube.com/watch?v=lbt2_M1hZeg)
+- [Web Crypto API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Crypto_API)
---
+++ script.js
@@ -0,0 +1,98 @@
+const buff_to_base64 = (buff) => btoa(String.fromCharCode.apply(null, buff));
+
+const base64_to_buf = (b64) =>
+  Uint8Array.from(atob(b64), (c) => c.charCodeAt(null));
+
+const enc = new TextEncoder();
+const dec = new TextDecoder();
+
+async function encrypt() {
+  const data = window.document.getElementById("data").value;
+  let encryptedDataOut = window.document.getElementById("encryptedData");
+  const password = window.prompt("Password");
+  const encryptedData = await encryptData(data, password);
+  encryptedDataOut.value = encryptedData;
+}
+
+async function decrypt() {
+  const password = window.prompt("Password");
+  const encryptedData = window.document.getElementById("encryptedData").value;
+  let decryptedDataOut = window.document.getElementById("decrypted");
+  const decryptedData = await decryptData(encryptedData, password);
+  decryptedDataOut.value = decryptedData || "decryption failed!";
+}
+
+const getPasswordKey = (password) =>
+  window.crypto.subtle.importKey("raw", enc.encode(password), "PBKDF2", false, [
+    "deriveKey",
+  ]);
+
+const deriveKey = (passwordKey, salt, keyUsage) =>
+  window.crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt: salt,
+      iterations: 250000,
+      hash: "SHA-256",
+    },
+    passwordKey,
+    { name: "AES-GCM", length: 256 },
+    false,
+    keyUsage
+  );
+
+async function encryptData(secretData, password) {
+  try {
+    const salt = window.crypto.getRandomValues(new Uint8Array(16));
+    const iv = window.crypto.getRandomValues(new Uint8Array(12));
+    const passwordKey = await getPasswordKey(password);
+    const aesKey = await deriveKey(passwordKey, salt, ["encrypt"]);
+    const encryptedContent = await window.crypto.subtle.encrypt(
+      {
+        name: "AES-GCM",
+        iv: iv,
+      },
+      aesKey,
+      enc.encode(secretData)
+    );
+
+    const encryptedContentArr = new Uint8Array(encryptedContent);
+    let buff = new Uint8Array(
+      salt.byteLength + iv.byteLength + encryptedContentArr.byteLength
+    );
+    buff.set(new Uint8Array(salt), 0);
+    buff.set(new Uint8Array(iv), salt.byteLength);
+    buff.set(
+      new Uint8Array(encryptedContentArr),
+      salt.byteLength + iv.byteLength
+    );
+    const base64Buff = buff_to_base64(buff);
+    return base64Buff;
+  } catch (e) {
+    console.log(`Error - ${e}`);
+    return "";
+  }
+}
+
+async function decryptData(encryptedData, password) {
+  try {
+    const encryptedDataBuff = base64_to_buf(encryptedData);
+    const salt = encryptedDataBuff.slice(0, 16);
+    const iv = encryptedDataBuff.slice(16, 16 + 12);
+    const data = encryptedDataBuff.slice(16 + 12);
+    const passwordKey = await getPasswordKey(password);
+    const aesKey = await deriveKey(passwordKey, salt, ["decrypt"]);
+    const decryptedContent = await window.crypto.subtle.decrypt(
+      {
+        name: "AES-GCM",
+        iv: iv,
+      },
+      aesKey,
+      data
+    );
+    return dec.decode(decryptedContent);
+  } catch (e) {
+    console.log(`Error - ${e}`);
+    return "";
+  }
+}
